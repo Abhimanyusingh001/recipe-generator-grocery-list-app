5f9f307a6cd53647550744b87385fdf45f02d877
Abhimanyusingh001 <143943718+Abhimanyusingh001@users.noreply.github.com>
2025-07-18 12:29:00 +0530

    Add .next/static/chunks/src_app_page_tsx_5c2ffce7._.js 1752821940338

---
+++ .next/static/chunks/src_app_page_tsx_5c2ffce7._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_5a9d5892._.js"
+],
+    source: "dynamic"
+});
