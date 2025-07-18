82b59f553005879029703dce829582d2e96fbb2b
Abhimanyusingh001 <143943718+Abhimanyusingh001@users.noreply.github.com>
2025-07-18 12:28:52 +0530

    Add .next/static/chunks/src_app_page_tsx_990dedc1._.js 1752821932147

---
+++ .next/static/chunks/src_app_page_tsx_990dedc1._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_21d5d1c5._.js"
+],
+    source: "dynamic"
+});
